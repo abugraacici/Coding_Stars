cb751d18293a5d952008710ffe88f962c8d85e3a
Abdulbaki YAKAR <abdulbaki.yakar0@gmail.com>
2025-07-04 02:32:05 +0300

    Delete SuperZone-BE-main directory

--- SuperZone-BE-main/README.md
+++
@@ -1,1 +0,0 @@
-# Abdulbaki Yakar-Backend eklendi
--- SuperZone-BE-main/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middleware/authMiddleware');
-const orderController = require('../controllers/orderController');
-
-router.get('/', verifyToken, orderController.getUserOrders);
-router.get('/:id', verifyToken, orderController.getOrderById);
-
-module.exports = router;
